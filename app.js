3a0ca99c82e6fbaaffe7174501b27e842581b638
victor.malakhau <victor.malakhau@elmosoft.net>
2025-07-27 23:23:20 +0300

    function

--- app.js
+++ app.js
@@ -1,14 +1,47 @@
-const balance = 1000;
-const actualBalance = prompt('Input your balance');
-const bonusBalance = 100;
-const actualBonusBalance = prompt('Input your bonus balance');
-const isBanned = false;
-const isExist = false;
-const isSelling = true;
+function logName() {
+	console.log(`My name is Victor`);
+}
 
-console.log(`Can I buy this game? ${(actualBalance > balance
-	|| actualBonusBalance > bonusBalance)
-	&& !isBanned
-	&& !isExist
-	&& isSelling
-	? 'YES' : 'NO'}`);
+const a = logName();
+console.log(a);//undefined
+
+function logName(name, surname) {
+	console.log(`My name is ${name} ${surname}`);
+}
+
+logName('Victor', 'Malahov');
+
+function countDepositSum(depositInUSD, month, rate) {
+	return depositInUSD * (1 + rate / 12) ** month;
+}
+
+const example1 = countDepositSum(1000, 24, 0.12);
+console.log(example1);
+
+console.log(countDepositSum(1000, 48, 0.10));
+
+//anonymous function
+
+function powerOfTwo(numb) {
+	return numb * numb;
+}
+
+console.log(powerOfTwo(5));
+
+const poft = function (numb) {
+	return numb * numb;
+}
+
+console.log(poft(6));
+
+//arrow function
+
+const poft2 = (num) => num * num;
+// const poft2 = num => num * num; //same
+
+const poft3 = num => {
+	console.log(num);
+	return num * num;
+};
+
+console.log(poft3(8));
