55d19bf59d794d5a06f52f3371ce5ea2219e1192
victor.malakhau <victor.malakhau@elmosoft.net>
2025-07-27 18:47:20 +0300

    exercise switch

--- app.js
+++ app.js
@@ -1,50 +1,11 @@
-const role = 'ceo';
-
-if (role === 'manager') {
-	console.log('Manager');
-} else if (role === 'admin') {
-	console.log('Admin');
-} else if (role === 'ceo') {
-	console.log('CEO');
-} else {
-	console.log('We don`t know you!');
-}
-
-switch (role) {
-	case 'manager':
-		console.log('Manager');
-		break;
-	case 'admin':
-		console.log('Admin');
-		break;
-	case 'ceo':
-		console.log('CEO');
-		break;
-	default:
-		console.log('We don`t know you!');
-}
-
-switch (role) {
-	case 'manager':
-	case 'admin':
-		console.log('Not superviser');
-		break;
-	case 'ceo':
-		console.log('Superviser');
-		break;
-	default:
-		console.log('We don`t know you!');
-}
-
-const numb = 1;
+const res = prompt('How much 7 + or - 15');
 
 switch (true) {
-	case numb > 0:
-		console.log('positive');
-		break;
-	case numb < 0:
-		console.log('negative');
+	case res === 'I am not a robot':
+	case Number(res) === 22:
+	case Number(res) === -8:
+		console.log('Success');
 		break;
 	default:
-		console.log('Zero!');
+		console.log('You are robot!!!');
 }
