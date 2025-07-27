2cafce6fa88a21fe050c688c5d0f54c86a3fd748
victor.malakhau <victor.malakhau@elmosoft.net>
2025-07-27 18:26:15 +0300

    switch

--- app.js
+++ app.js
@@ -1,14 +1,50 @@
-const secretNumber = '7';
+const role = 'ceo';
 
-if (secretNumber == 7) {
-	console.log('equal exclude type')
+if (role === 'manager') {
+	console.log('Manager');
+} else if (role === 'admin') {
+	console.log('Admin');
+} else if (role === 'ceo') {
+	console.log('CEO');
+} else {
+	console.log('We don`t know you!');
 }
 
-if (Number(secretNumber) === 7) {
-	console.log('equal include type and value')
+switch (role) {
+	case 'manager':
+		console.log('Manager');
+		break;
+	case 'admin':
+		console.log('Admin');
+		break;
+	case 'ceo':
+		console.log('CEO');
+		break;
+	default:
+		console.log('We don`t know you!');
 }
 
-const q = prompt('Input number');
-if (Number(q) === 7) {
-	console.log('equal exclude type')
+switch (role) {
+	case 'manager':
+	case 'admin':
+		console.log('Not superviser');
+		break;
+	case 'ceo':
+		console.log('Superviser');
+		break;
+	default:
+		console.log('We don`t know you!');
+}
+
+const numb = 1;
+
+switch (true) {
+	case numb > 0:
+		console.log('positive');
+		break;
+	case numb < 0:
+		console.log('negative');
+		break;
+	default:
+		console.log('Zero!');
 }
