3a851b3bb6ff73cf4c6a9cf6b141891fab12a170
victor.malakhau <victor.malakhau@elmosoft.net>
2025-07-29 13:08:18 +0300

    array part1

--- app.js
+++ app.js
@@ -1,31 +1,21 @@
-const age = 25;
-const isWork = false;
-const money = 1910;
-const price = 2000;
+const roles = ['admin', 'user', 'superuser'];
+const userInfo = ['Anna', 25];
+console.log(userInfo);
+console.log(roles[0]);
+console.log(roles.length);
+console.log(roles[roles.length - 1]);
 
-function getCredit(age, isWork = false) {
-	if (age > 24 && isWork) {
-		console.log('can get 500$ in credit');
-		return 500;
-	} else if (age > 24) {
-		console.log('can get 100$ in credit');
-		return 100;
-	}
-	console.log('credit denied');
-	return 0;
+console.log(roles.at(0));
+console.log(roles.at(-1));
+
+const userName = new Array('Vasia', 'Petia', 'Katia');
+console.log(userName);
+
+const userAge = [2040 - 2022, 10 > 0 ? 5 : 0];
+console.log(userAge);
+
+function square(el1) {
+	return el1 * el1;
 }
 
-function canBuyMacBook(age, isWork, money, price) {
-	if (money > price) {
-		console.log('without credit');
-		return true;
-	}
-	money += getCredit(age, isWork);
-	if (money > price) {
-		console.log('with credit');
-		return true;
-	}
-	return false;
-}
-
-console.log(`Can I buy MacBook ${canBuyMacBook(age, isWork, money, price)}`);
+console.log(square([1, 2, 3]));//NaN
