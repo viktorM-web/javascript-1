54687b87a37a216280ede5917a640d342060b7f5
victor.malakhau <victor.malakhau@elmosoft.net>
2025-07-29 14:53:39 +0300

    slice, splice, concat, reverse

--- app.js
+++ app.js
@@ -1,21 +1,42 @@
-const users = ['Anna', 'Vika', 'Katia'];
-console.log(users);
-users[2] = 'Kristina';
-console.log(users);
-users[4] = 'Nikita';
-console.log(users);//['Anna', 'Vika', 'Kristina', empty, 'Nikita']
+const roles = ['user', 'admin', 'manager', 'superuser'];
 
-const arrayLength = users.push('Evgenii');
-console.log(users);
-console.log(arrayLength);
+const result = roles.slice(2);
+console.log(roles);
+console.log(result);//['manager', 'superuser']
 
-console.log(users.unshift('Vasia'));
-console.log(users);
 
-const lastElement = users.pop();
-console.log(lastElement);
-console.log(users);
+const result2 = roles.slice(2, 3);
+console.log(roles);
+console.log(result2);//['manager']
 
-const firstElement = users.shift();
-console.log(firstElement);
-console.log(users);
+const result3 = roles.slice(-1);
+console.log(roles);
+console.log(result3);//['superuser']
+
+const result4 = roles.slice(1, -2);
+console.log(roles);
+console.log(result4);//['admin']
+
+console.log(roles);//['user', 'admin', 'manager', 'superuser']
+
+// const result5 = roles.splice(2);
+// console.log(result5); // ['manager', 'superuser']
+// console.log(roles); //['user', 'admin']
+
+// const result5 = roles.splice(2, 1);
+// console.log(result5); // ['manager']
+// console.log(roles); //['user', 'admin', 'superuser']
+
+// const result5 = roles.splice(-1);
+// console.log(result5); // ['superuser']
+// console.log(roles); //['user', 'admin', 'manager']
+
+const result5 = roles.reverse();
+console.log(result5);//['superuser', 'manager', 'admin', 'user']
+console.log(roles); //['superuser', 'manager', 'admin', 'user']
+
+const newRoles = ['sysadmin', 'developer'];
+const result6 = roles.concat(newRoles);
+console.log(result6);//['superuser', 'manager', 'admin', 'user', 'sysadmin', 'developer']
+console.log(roles);
+console.log(newRoles);
