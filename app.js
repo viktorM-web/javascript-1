a2b6de98df8b9037d53c2a262a8a53afb339707f
victor.malakhau <victor.malakhau@elmosoft.net>
2025-08-12 18:57:01 +0300

    higher-order functions or callback

--- app.js
+++ app.js
@@ -1,35 +1,26 @@
-const operations = [1000, -700, 300, -500, 10000];
-let initailBalance = 100;
-
-printAverege();
-
-console.log(`Final balance ${getFinalBalance(initailBalance, operations)}`);
-
-function getFinalBalance(balance, listOperations) {
-	for (let operation of listOperations) {
-		balance += operation;
-		if (balance < 0) {
-			return false;
-		}
-	}
-	return balance;
+function add(a, b) {
+	return a + b;
 }
 
-function printAverege() {
-	let countPositiveOper = 0;
-	let countNegativeOper = 0;
-	let sumNegotiveOper = 0;
-	let sumPositiveOper = 0;
-
-	for (let operation of operations) {
-		if (operation > 0) {
-			countPositiveOper++;
-			sumPositiveOper += operation;
-		} else {
-			countNegativeOper++;
-			sumNegotiveOper -= operation;
-		}
-	}
-	console.log(`Average expense ${sumNegotiveOper / countNegativeOper}`);
-	console.log(`Average income ${sumPositiveOper / countPositiveOper}`);
+function subtract(a, b) {
+	return a - b;
 }
+
+function power(a, b) {
+	return a ** b;
+}
+
+//higher-order functions or callback
+
+function calculate(a, b, fn) {
+	console.log(fn.name);
+	const res = fn(a, b);
+	return res;
+}
+
+let res = calculate(3, 5, add);
+console.log(res);
+res = calculate(3, 5, subtract);
+console.log(res);
+res = calculate(3, 5, power);
+console.log(res);
