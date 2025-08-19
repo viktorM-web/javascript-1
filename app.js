34035c8b556128cd4f9a19a2b9a1b777bf289c88
victor.malakhau <victor.malakhau@elmosoft.net>
2025-08-19 23:02:16 +0300

    array into string and string into array

--- app.js
+++ app.js
@@ -1,37 +1,9 @@
-const num1 = '89103235356';
-const num2 = '+79103235356';
-const num3 = '+7(910)3235356';
-const num4 = '+7(910) 323-53-56';
-const num5 = '	+7(910) 323-53-56 ';
+const userFullName = 'Vasia Pupkin Ivanovich';
+console.log(userFullName.split(' '));
+const [firstName, familyName, lastName] = userFullName.split(' ');
+console.log(firstName);//Vasia
+console.log(familyName);//Pupkin
+console.log(lastName);//Ivanovich
 
-const num6 = '8910323535';
-const num7 = '+7d910d323-53-5';
-const num8 = '9+7d910d323-53-5';
-const num9 = '89103g23535';
-
-function isphoneNumber(maybePhoneNumber) {
-	const cleanMaybePhoneNumber = maybePhoneNumber.replaceAll('(', '')
-		.replaceAll(')', '')
-		.replaceAll(' ', '')
-		.replaceAll('-', '')
-		.trim();
-
-	console.log(cleanMaybePhoneNumber);
-	if (cleanMaybePhoneNumber.startsWith('8') && cleanMaybePhoneNumber.length == 11) {
-		return !isNaN(cleanMaybePhoneNumber);
-	} else if (cleanMaybePhoneNumber.startsWith('+7') && cleanMaybePhoneNumber.length == 12) {
-		return !isNaN(cleanMaybePhoneNumber.replace('+', ''));
-	} else {
-		return false;
-	}
-}
-
-console.log(isphoneNumber(num1));
-console.log(isphoneNumber(num2));
-console.log(isphoneNumber(num3));
-console.log(isphoneNumber(num4));
-console.log(isphoneNumber(num5));
-console.log(isphoneNumber(num6));
-console.log(isphoneNumber(num7));
-console.log(isphoneNumber(num8));
-console.log(isphoneNumber(num9));
+const arr = ['You', 'know', 'JS'];
+console.log(arr.join(' '));//You know JS
