bd09be897caf58d5bb205135e1a7c1eb6e10bec3
victor.malakhau <victor.malakhau@elmosoft.net>
2025-08-19 22:53:18 +0300

    exercise check phone number

--- app.js
+++ app.js
@@ -1,17 +1,37 @@
-const userName = 'Vasya Pupkina';
+const num1 = '89103235356';
+const num2 = '+79103235356';
+const num3 = '+7(910)3235356';
+const num4 = '+7(910) 323-53-56';
+const num5 = '	+7(910) 323-53-56 ';
 
-console.log(userName.includes('a'));//true
-console.log(userName.startsWith('V'));//true
-console.log(userName.endsWith('na'));//true
+const num6 = '8910323535';
+const num7 = '+7d910d323-53-5';
+const num8 = '9+7d910d323-53-5';
+const num9 = '89103g23535';
 
-console.log(userName.toLowerCase());//vasya pupkina
-console.log(userName.toUpperCase());//VASYA PUPKINA
-console.log(userName.replace('V', 'W'));//Wasya Pupkina
-console.log(userName.replace('P', 'Z'));//Vasya Zupkina
+function isphoneNumber(maybePhoneNumber) {
+	const cleanMaybePhoneNumber = maybePhoneNumber.replaceAll('(', '')
+		.replaceAll(')', '')
+		.replaceAll(' ', '')
+		.replaceAll('-', '')
+		.trim();
 
-console.log(userName.replace('a', 'i'));//Visya Pupkina
-console.log(userName.replaceAll('a', 'i'));//Visyi Pupkini
-console.log(userName.replace(/a/g, 'i'));//Visyi Pupkini
+	console.log(cleanMaybePhoneNumber);
+	if (cleanMaybePhoneNumber.startsWith('8') && cleanMaybePhoneNumber.length == 11) {
+		return !isNaN(cleanMaybePhoneNumber);
+	} else if (cleanMaybePhoneNumber.startsWith('+7') && cleanMaybePhoneNumber.length == 12) {
+		return !isNaN(cleanMaybePhoneNumber.replace('+', ''));
+	} else {
+		return false;
+	}
+}
 
-const str = '	dfdfd dfdf \n';
-console.log(str.trim());//dfdfd dfdf
+console.log(isphoneNumber(num1));
+console.log(isphoneNumber(num2));
+console.log(isphoneNumber(num3));
+console.log(isphoneNumber(num4));
+console.log(isphoneNumber(num5));
+console.log(isphoneNumber(num6));
+console.log(isphoneNumber(num7));
+console.log(isphoneNumber(num8));
+console.log(isphoneNumber(num9));
