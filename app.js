606d169e35581e9103a893e588d54a2b419d25ce
victor.malakhau <victor.malakhau@elmosoft.net>
2025-08-23 15:59:15 +0300

    final exercise with object

--- app.js
+++ app.js
@@ -1,18 +1,45 @@
-const cities = {
-	msk: {
-		temp: {
-			celsius: 25
-		}
+const warehouse = {
+	goods: [],
+	findGoodById: function (id) {
+		return this.goods.find(g => g.id == id);
 	},
-	spb: {
+	addGood: function (good) {
+		if (this.findGoodById(good.id)) {
+			console.log('This good is alrady on the warehouse');
+			return;
+		}
+		this.goods.push(good);
+	},
+	getWeight: function () {
+		return this.goods.reduce(
+			(weight, good) => weight += good?.weight?.kg ? good.weight.kg : 0
+			, 0);
 
 	}
-};
-
-const city = 'spb';
-
-if (cities[city] != undefined && cities[city].temp != undefined) {
-	console.log(cities[city].temp.celsius)
 }
 
-console.log(cities[city]?.temp?.celsius)
+const car = {
+	id: 1,
+	weight: {
+		kg: 1000
+	},
+	brand: 'Ford'
+};
+const chair = {
+	id: 3,
+	weight: {
+		kg: 2
+	}
+};
+const paper = {
+	id: 5,
+	color: 'red'
+};
+
+warehouse.addGood(car);
+warehouse.addGood(car);
+warehouse.addGood(chair);
+warehouse.addGood(paper);
+
+console.log(warehouse.findGoodById(3));
+console.log(warehouse.getWeight());
