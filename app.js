b079b8c4352fefa72d22206b2c8d87b25e14d7e4
victor.malakhau <victor.malakhau@elmosoft.net>
2025-08-23 14:24:12 +0300

    destructorization and rest

--- app.js
+++ app.js
@@ -1,27 +1,21 @@
-const cities = {
-	msk: {
-		let: 200,
-		tem: 25
-	},
-	spb: {
-		let: 100,
-		tem: 20
-	}
+const user = {
+	name: 'Vasia',
+	age: 40,
+	city: 'Moscow'
 };
 
-let sumTemp = 0;
-let citiesCount = 0;
+const { age, ...userWithoutAge } = user;
+console.log(age);
+console.log(userWithoutAge);
 
-for (const key in cities) {
-	console.log(key);
-	citiesCount++;
-	sumTemp += cities[key].tem;
-}
-console.log(sumTemp / citiesCount);
+const additionalData = {
+	skills: ['Development', 'Design'],
+	creditcard: '2342-2345-2734-2356'
+};
 
-sumTemp = 0;
-citiesCount = Object.keys(cities).length;
-for (const key of Object.keys(cities)) {
-	sumTemp += cities[key].tem
-}
-console.log(sumTemp / citiesCount);
+const userWithAdditionalData = {
+	...user,
+	...additionalData
+};
+
+console.log(userWithAdditionalData);
