176c2bc1f4ef68431bd89b666bf0d10a40eae92c
victor.malakhau <victor.malakhau@elmosoft.net>
2025-08-24 19:52:51 +0300

    IIFE

--- app.js
+++ app.js
@@ -1,18 +1,3 @@
-'use strict'
-
-const user = {
-	login: 'Audi@google.com',
-	password: 'A3$$TYH'
-};
-
-function removePassword(reset) {
-	if (reset) {
-		this.password = undefined;
-	} else {
-		this.password = '1';
-	}
-}
-
-const resetUserPessword = removePassword.bind(user, true);
-resetUserPessword();
-console.log(user);
+(function () {
+	console.log('start IIFO');
+})();
