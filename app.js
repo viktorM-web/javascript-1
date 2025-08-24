a239e6adb916957fdf0f6da782cb9a01122f18d2
victor.malakhau <victor.malakhau@elmosoft.net>
2025-08-24 20:16:34 +0300

    exercise with closer

--- app.js
+++ app.js
@@ -1,3 +1,52 @@
-(function () {
-	console.log('start IIFO');
-})();
+'user strict'
+
+const userInfo = {
+	balance: 0,
+	operations: 0,
+	increse(sum) {
+		this.balance += sum;
+		this.operations++;
+	},
+	blueprint() {
+		return () => {
+			let balance = this.balance;
+			let operations = this.operations;
+			let increse = this.increse;
+
+			return {
+				balance,
+				operations,
+				increse
+			}
+		}
+	}
+}
+
+const user1 = userInfo.blueprint()();
+console.log(user1);
+user1.increse(100);
+console.log(user1);
+console.log(userInfo);
+
+function createUser() {
+	const userObj = {
+		balance: 0,
+		operations: 0,
+		increse(sum) {
+			this.balance += sum;
+			this.operations++;
+		}
+	};
+	return () => {
+		return userObj;
+	}
+}
+
+const user2 = createUser();
+user2().increse(200);
+user2().increse(300);
+console.log(user2());
+
+const user4 = createUser();
+user4().increse(350);
+console.log(user4());
