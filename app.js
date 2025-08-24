335982774a2d9c25bcfbc28f2a309a87d995aad4
victor.malakhau <victor.malakhau@elmosoft.net>
2025-08-24 19:25:26 +0300

    exercise management this

--- app.js
+++ app.js
@@ -1,44 +1,18 @@
 'use strict'
 
-const audi = {
-	brand: 'Audi',
-	model: 'A3',
-	damages: []
+const user = {
+	login: 'Audi@google.com',
+	password: 'A3$$TYH'
 };
 
-
-const carManipulation = {
-	addDamage(part, rate) {
-		this.damages.push({ part, rate });
-		console.log(`add damage for ${this.brand} ${this.model}`);
+function removePassword(reset) {
+	if (reset) {
+		this.password = undefined;
+	} else {
+		this.password = '1';
 	}
-};
+}
 
-const addDamageAudi = carManipulation.addDamage.bind(audi);
-
-addDamageAudi('front', 9);//add damage for Audi A3
-console.log(audi);
-
-const addBodyDamageAudi = carManipulation.addDamage.bind(audi, 'body');
-addBodyDamageAudi(3);//add damage for Audi A3
-console.log(audi);
-/*
-brand
-: 
-"Audi"
-damages
-: 
-Array(2)
-0
-: 
-{part: 'front', rate: 9}
-1
-: 
-{part: 'body', rate: 3}
-length
-: 
-2
-[[Prototype]]
-: 
-Array(0
-*/
+const resetUserPessword = removePassword.bind(user, true);
+resetUserPessword();
+console.log(user);
