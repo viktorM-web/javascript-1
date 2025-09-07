c4ae70c349f660f25ac3559832e3f3659f86c867
victor.malakhau <victor.malakhau@elmosoft.net>
2025-09-07 21:22:32 +0300

    key event handler

--- app.js
+++ app.js
@@ -9,13 +9,30 @@
 // 	document.querySelector('.input').value = '';
 // });
 
-function changeClick() {
-	document.querySelector('.button').addEventListener('click', function () {
-		const input = document.querySelector('.input').value;
-		if (!input) {
-			return;
-		}
-		document.querySelector('.panel').innerText = input;
-		document.querySelector('.input').value = '';
-	});
+function submitForm() {
+	const input = document.querySelector('.input').value;
+	if (!input) {
+		return;
+	}
+	document.querySelector('.panel').innerText = input;
+	document.querySelector('.input').value = '';
 };
+
+function changeClick() {
+	submitForm();
+}
+
+function submitByEnter(e) {
+	if (e.code == 'Enter') {
+		submitForm();
+	}
+};
+// function submitByEnter() {
+// 	document.querySelector('.input').addEventListener('keydown', (e) => {
+// 		if (e.code == 'Enter') {
+// 			submitForm();
+// 		}
+// 	});
+// }
+
+
