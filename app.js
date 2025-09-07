c869c027a3e1395629d69dbf857c25ddc5c33b08
victor.malakhau <victor.malakhau@elmosoft.net>
2025-09-07 21:52:19 +0300

    setting attributes

--- app.js
+++ app.js
@@ -18,7 +18,14 @@ function submitForm() {
 	document.querySelector('.input').value = '';
 	// document.querySelector('.notification').style.display = 'block';
 	// document.querySelector('.notification').classList.add('notification_active');
-	document.querySelector('.notification').classList.remove('notification_disactive');
+
+	// document.querySelector('.notification').classList.remove('notification_disactive');
+
+	console.log(document.querySelector('.notification').getAttribute('class'));
+	document.querySelector('.notification').setAttribute('class', 'notification');
+	document.querySelector('.notification').setAttribute('key', 1);
+	document.querySelector('.notification').setAttribute('user-id', 1);
+	console.log(Number(document.querySelector('.notification').getAttribute('user-id')));
 };
 
 function changeClick() {
