39d53eb4218d95f3f3e33e071b826a7daf7e2f9d
victor.malakhau <victor.malakhau@elmosoft.net>
2025-09-14 20:27:59 +0300

    exercise get elements from DOM

--- app.js
+++ app.js
@@ -45,4 +45,12 @@ function submitByEnter(e) {
 // 	});
 // }
 
+// console.log(document.querySelector('.one > span').innerText)
+console.log(document.querySelectorAll('.one')[0].innerText)
+// console.log(document.querySelector('.one ~ div').innerText)
+console.log(document.querySelectorAll('.one')[1].innerText)
+
+// console.log(document.querySelector('#two').innerText)
+console.log(document.getElementById('two').innerText)
+console.log(document.querySelector('[user-id="4"]').innerText)
 
